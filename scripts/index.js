7a2cefc71097eeaab2eb390e9d7bfba1e1d77bc1
Eliot King <eliotaking@gmail.com>
2023-12-06 16:58:56 -0500

    Add initial cards JavaScript

---
+++ scripts/index.js
@@ -0,0 +1,34 @@
+// Card objects
+let yosemite = {
+  name: "Yosemite Valley",
+  link: "./images/yosemite.jpg",
+};
+let lakeLouise = {
+  name: "Lake Louise",
+  link: "./images/lake-louise.jpg",
+};
+let baldMountains = {
+  name: "Bald Mountains",
+  link: "./images/bald-mountains.jpg",
+};
+let latemar = {
+  name: "Latemar",
+  link: "./images/latemar.jpg",
+};
+let vanoise = {
+  name: "Vanoise National Park",
+  link: "./images/vanoise.jpg",
+};
+let lagoDiBraies = {
+  name: "Lago di Braies",
+  link: "./images/lago-di-braies.jpg",
+};
+// Cards array
+let initialCards = [
+  yosemite,
+  lakeLouise,
+  baldMountains,
+  latemar,
+  vanoise,
+  lagoDiBraies,
+];
