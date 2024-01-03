7204113eb45175f1bcc22e09365625f422017755
Eliot King <eliotaking@gmail.com>
2024-01-03 18:53:47 -0500

    move card creation to JavaScript

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,7 @@
+//===================
+// Card Functionality
+//===================
+
 // Card objects
 let yosemite = {
   name: "Yosemite Valley",
@@ -23,6 +27,7 @@ let lagoDiBraies = {
   name: "Lago di Braies",
   link: "./images/lago-di-braies.jpg",
 };
+
 // Cards array
 let initialCards = [
   yosemite,
@@ -32,6 +37,37 @@ let initialCards = [
   vanoise,
   lagoDiBraies,
 ];
+
+// Loops through each object in the cards array, calls getCardElement on them, and adds them to the DOM
+for (let i = 0; i < initialCards.length; i++) {
+  let cards = document.querySelector(".gallery__cards");
+  let cardToAdd = getCardElement(initialCards[i], i);
+  cards.append(cardToAdd);
+}
+
+function getCardElement(data) {
+  // Assigns the properties from the card objects to variables
+  let cardName = data.name;
+  let cardLink = data.link;
+  // Selects the card template and clones it into a variable "card"
+  let card = document
+    .querySelector("#card-template")
+    .content.querySelector(".card")
+    .cloneNode(true);
+  // Selects the relevant card sub-elements and assigns them into variables
+  let cardImage = card.querySelector(".card__image");
+  let cardTitle = card.querySelector(".card__title");
+
+  // Sets the card values and returns the card
+  cardImage.setAttribute("src", cardLink);
+  cardImage.setAttribute("alt", cardName);
+  cardTitle.textContent = cardName;
+  return card;
+}
+
+//===========================
+// Edit Profile Functionality
+//===========================
 
 // Button variables
 let editButton = document.querySelector(".profile__edit-button");
